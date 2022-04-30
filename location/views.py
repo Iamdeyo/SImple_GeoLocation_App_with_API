a9943b9efd4a0d623eb6f2d19f904b88125cf4aa
Deyo <dtechlord@gmail.com>
2022-04-30 13:31:19 +0100

    Final

--- location/views.py
+++ location/views.py
@@ -9,14 +9,20 @@ import json
 
 def index(request):
 
-    
+    # to receive user ip data from the API
+    getUserIp = requests.get("https://api.ipify.org?format=json")
+    # to convert the file from JSON to py dictionary 
+    getUserIp_data = json.loads(getUserIp.text)
+    # access the ip in dictionary
+    Ip_data = getUserIp_data['ip']
 
-    # to get receive data from the API
-    geoLoc = requests.get("http://ip-api.com/json/24.48.0.1")
 
+
+    # to receive data from the API
+    geoLoc = requests.get("http://ip-api.com/json/" + Ip_data)
     # to convert the file from JSON to py dictionary  
     geoLoc_data_text = geoLoc.text
     geoLoc_data = json.loads(geoLoc_data_text)
 
 
-    return render(request, 'location/index.html', {'data': geoLoc_data})
+    return render(request, 'location/index.html', {'data': geoLoc_data,})

e29e5d3fc5beb4d2fd373c0e7cc69f4369ecf39b
Deyo <dtechlord@gmail.com>
2022-04-30 10:32:31 +0100

    static location data

--- location/urls.py
+++ location/urls.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
+
 from django.urls import path
 from .views import index
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
 ]
--- location/views.py
+++ location/views.py
@@ -1,10 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+import requests
+import json
 
 # Create your views here.
 # def test(req):
 #     return HttpResponse("Hello Moto")
 
 def index(request):
-    return render(request, 'location/index.html', {})
+
+    
+
+    # to get receive data from the API
+    geoLoc = requests.get("http://ip-api.com/json/24.48.0.1")
+
+    # to convert the file from JSON to py dictionary  
+    geoLoc_data_text = geoLoc.text
+    geoLoc_data = json.loads(geoLoc_data_text)
+
+
+    return render(request, 'location/index.html', {'data': geoLoc_data})
